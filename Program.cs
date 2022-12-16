b0dd37e62c82954a21e45d62430962b3fb71a034
Oliver <oliver721003@gmail.com>
2022-12-17 01:31:39 +0800

    feat: setting connection configuration

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<BookContext>(
-	options => options.UseSqlServer("Server=localhost,1433;Database=BookDemo;TrustServerCertificate=True;User=sa;Password=Zaq1Xsw2;")
+	options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
