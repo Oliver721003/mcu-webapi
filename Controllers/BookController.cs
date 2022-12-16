517c301c2b5c3d15302b47604b0a1703855af53d
Oliver <oliver721003@gmail.com>
2022-12-17 00:38:08 +0800

    feat: crud from and to database

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -7,49 +7,39 @@ namespace WebApiDemo.Controllers;
 [Route("books")]
 public class BookController : ControllerBase
 {
-	private static List<Book> _books = new List<Book> {
-		new Book { Id = 1, Name = "Book 1", Author = "Author 1", Company = "Company 1" },
-		new Book { Id = 2, Name = "Book 2", Author = "Author 2", Company = "Company 2" },
-		new Book { Id = 3, Name = "Book 3", Author = "Author 3", Company = "Company 3" },
-		new Book { Id = 4, Name = "Book 4", Author = "Author 4", Company = "Company 4" },
-		new Book { Id = 5, Name = "Book 5", Author = "Author 5", Company = "Company 5" },
-	};
-
 	[HttpGet]
-	public IEnumerable<Book> Get()
+	public IEnumerable<Book> Get(string? name = "")
 	{
-		return _books;
+		var db = new BookContext();
+		var books = db.Books.AsQueryable();
+		if (!string.IsNullOrEmpty(name))
+			books = books.Where(x => x.Name.Contains(name));
+		return books.ToList();
 	}
 
 	[HttpGet]
 	[Route("{id}")]
 	public Book Get(int id)
 	{
-		return _books.FirstOrDefault(b => b.Id == id);
+		var db = new BookContext();
+		return db.Books.Find(id);
 	}
 
 	[HttpPost]
 	public IActionResult Insert([FromBody] Book book)
 	{
-		var id = _books.Any() ? _books.Max(b => b.Id) + 1 : 1;
-		book.Id = id;
-		_books.Add(book);
+		var db = new BookContext();
+		db.Add(book);
+		db.SaveChanges();
 		return Ok();
 	}
 
 	[HttpPut]
-	[Route("{id}")]
-	public IActionResult Update([FromRoute] int id, [FromBody] Book book)
+	public IActionResult Update([FromBody] Book book)
 	{
-		var bookToUpdate = _books.FirstOrDefault(b => b.Id == id);
-		if (bookToUpdate == null)
-		{
-			return NotFound();
-		}
-
-		bookToUpdate.Name = book.Name;
-		bookToUpdate.Author = book.Author;
-		bookToUpdate.Company = book.Company;
+		var db = new BookContext();
+		db.Update(book);
+		db.SaveChanges();
 		return Ok();
 	}
 
@@ -57,7 +47,9 @@ public class BookController : ControllerBase
 	[Route("{id}")]
 	public IActionResult Delete([FromRoute] int id)
 	{
-		_books.RemoveAll(book => book.Id == id);
+		var db = new BookContext();
+		db.Remove(new Book { Id = id });
+		db.SaveChanges();
 		return Ok();
 	}
 }
